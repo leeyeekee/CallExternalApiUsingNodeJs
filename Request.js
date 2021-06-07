8c14db33f70555c4539e9b0bd8ce3697aea4b79d
leeyeekee <leeyeekee@hotmail.com>
2021-06-07 20:05:48 +0800

    Update Request.js

--- Request.js
+++ Request.js
@@ -1,6 +1,6 @@
 const request = require('request');
  
-_EXTERNAL_URL = 'https://test-proj-heroku.herokuapp.com/api/plans';
+_EXTERNAL_URL = 'http://studentportal-sample.2886795276-80-jago05.environments.katacoda.com/students';
 
 const callExternalApiUsingRequest = (callback) => {
     request(_EXTERNAL_URL, { json: true }, (err, res, body) => {
