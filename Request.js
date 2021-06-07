08db8239283487399dd9132c37d2bb9b4e249295
leeyeekee <leeyeekee@hotmail.com>
2021-06-07 21:30:12 +0800

    Update Request.js

--- Request.js
+++ Request.js
@@ -1,7 +1,8 @@
 const request = require('request');
+// var targetRestURL = process.env.TARGET
+// _EXTERNAL_URL = 'http://studentportal-sample.2886795281-80-jago05.environments.katacoda.com/students';
+_EXTERNAL_URL = process.env.TARGET
  
-_EXTERNAL_URL = 'http://studentportal-sample.2886795281-80-jago05.environments.katacoda.com/students';
-
 const callExternalApiUsingRequest = (callback) => {
     request(_EXTERNAL_URL, { json: true }, (err, res, body) => {
     if (err) { 
